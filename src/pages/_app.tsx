608a9eeb34ba697ffa33ccbd15f81c9cffad6dae
Syed-Internexxus <syed.umer@internexxus.com>
2025-05-20 23:33:55 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 // pages/_app.tsx
 import "@/styles/globals.css";
-import InterviewPage from "./interviews"; // Direct import of the interviews page
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
-export default function App() {
-  return <InterviewPage />;
+export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.pathname !== "/interviews") {
+      router.replace("/interviews");
+    }
+  }, [router]);
+
+  return <Component {...pageProps} />;
 }

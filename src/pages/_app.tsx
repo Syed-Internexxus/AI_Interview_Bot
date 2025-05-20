603b6417398d2676d0119c6510757d26f5456bf1
Syed-Internexxus <syed.umer@internexxus.com>
2025-05-20 23:40:21 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,6 @@
-// pages/_app.tsx
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    if (router.pathname !== "/interviews") {
-      router.replace("/interviews");
-    }
-  }, [router]);
-
   return <Component {...pageProps} />;
 }
